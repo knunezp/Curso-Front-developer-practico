031bcb44ebc89cd623e04f58c18ed0f4fb2dfa0e
Karen Nuñez <knuez2013@gmail.com>
2023-06-20 13:27:29 -0600

    feat: :art:
    add product detail

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon= document.querySelector(".product-detail-close");
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener("click",toggleDesktopMenu);
 menuHamIcon.addEventListener("click",toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
   const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -28,6 +31,7 @@ function toggleMobileMenu() {
     shoppingCartContainer.classList.add('inactive');
   }
 
+  closeProductDetailAside();
   mobileMenu.classList.toggle('inactive');
 }
 
@@ -36,6 +40,12 @@ function toggleCarritoAside() {
 
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
+  }
+
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive');
   }
 
   shoppingCartContainer.classList.toggle('inactive');
@@ -65,6 +75,16 @@ productList.push(
   }
 );
 
+function openProductdDetailAside(){
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(){
+  productDetailContainer.classList.add("inactive");
+}
+
+
 function renderProduct(arr){
   for(product of arr){
     const productCard=document.createElement("div");
@@ -72,6 +92,7 @@ function renderProduct(arr){
 
     const productImage=document.createElement("img");
     productImage.setAttribute("src",product.image);
+    productImage.addEventListener("click",openProductdDetailAside);
 
     const productInfo=document.createElement("div");
     productInfo.classList.add("product-info");
